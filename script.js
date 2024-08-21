2b767b31e6d5625bfbfaba10dd12cd065110b988
RocioTCoria <rociotcoria@gmail.com>
2024-08-20 21:39:13 -0300

    Primer ejercicio

--- script.js
+++ script.js
@@ -16,3 +16,38 @@ window.onload=function(){
 
 }
 
+window.onload=function(){
+
+    var pantallacarga = document.getElementById("carga");
+
+        pantallacarga.style.display="block";
+
+        setTimeout (() => {
+
+            pantallacarga.style.display="none";
+
+        }, 1000);
+
+    var pantallaintermedia = document.getElementById("intermedio");
+    
+        pantallaintermedia.style.display="none";
+
+        setTimeout (() => {
+
+            pantallaintermedia.style.display="block";
+
+        }, 1000);
+
+    var chauboton = document.getElementById("salir");
+    var pantallaprincipal = document.getElementById("principal");
+    chauboton.addEventListener('click', function(){
+
+        pantallaintermedia.style.display="none";
+
+    });
+
+        
+}
+
+
+
