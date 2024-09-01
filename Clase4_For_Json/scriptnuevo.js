6f45eef8d546c97452c22c4e204fd5f54bea2e56
RocioTCoria <rociotcoria@gmail.com>
2024-09-01 00:08:50 -0300

    Clase tal cual

--- Clase4_For_Json/scriptnuevo.js
+++ Clase4_For_Json/scriptnuevo.js
@@ -17,9 +17,35 @@ var alumnos = [{
 }
 ]
 
-for (let index = 0; index < alumnos.length; index++) {
+
+
+window.onload=function(){
+
+    var contenedor = document.getElementById("contenedor");
+
+    for (let index = 0; index < alumnos.length; index++) {
+    // crear un h1 o algo en java //
+    var titulo = document.createElement("h1");
+    titulo.textContent = alumnos[index].nombre;
+    titulo.classList.add("nombrecss"); // para poder darle diseño en css
     
-    alert("nombre: " + alumnos[index].nombre + " apellido: " + alumnos[index].apellido)
+    var segundotitulo = document.createElement("h2");
+    segundotitulo.textContent = alumnos[index].apellido;
+    segundotitulo.classList.add("apellidocss");
+
+    var section = document.createElement("section");// crea un contenedor mas chiquito
+    section.classList.add("contenedorcito");
+
+    section.appendChild(titulo); // titulo y segtitulo quedan dentro del SECTION
+    section.appendChild(segundotitulo); 
+    contenedor.appendChild(section); // se agrega dentro del main (en index.html)
+
+    // alert("nombre: " + alumnos[index].nombre + " apellido: " + alumnos[index].apellido)
+
+    var marcar = document.createElement("input");
+    marcar.type="checkbox";
+    section.appendChild(marcar);
+} 
 }
 
 // .length = devuelve la cantidad de elementos
