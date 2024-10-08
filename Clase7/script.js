99f9cb3bede8e9bb555df04f0ba6848c3b6ff543
R. T. Coria <rociotcoria@gmail.com>
2024-10-08 19:08:18 -0300

    datos

--- Clase7/script.js
+++ Clase7/script.js
@@ -1,1 +1,9 @@
-fetch('')
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => {
+        var marco = document.getElementById("foto");
+        var imagen = document.createElement("img");
+        imagen.src = data.message; // 
+        marco.appendChild(imagen);})
+    .catch(error=>console.error('Error:', error));
+
